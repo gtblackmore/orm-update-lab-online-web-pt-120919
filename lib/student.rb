09f58f9f1fa067ad31596dfa5fa6ef66b5298329
George Blackmore <gtblackmore@gmail.com>
2020-02-21 00:09:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,27 +4,55 @@ class Student
 
   def initialize(name, grade, id=nil)
     @id = id
+    @name = name
+    @grade = grade
   end
   
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+      DB[:conn].execute(sql)
   end
   
   def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+      SQL
+    DB[:conn].execute(sql)
   end
   
   def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, name, grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   def self.create
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
   def self.new_from_db
+    
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)
   end
   
   def update
+    sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
   end
 
 end
